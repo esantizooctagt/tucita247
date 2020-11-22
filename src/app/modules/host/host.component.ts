09629896d7696afdf3031c8c1fdf3aefa26f495f
Erick Santizo <esantizo@octagt.com>
2020-11-22 12:02:14 -0600

    reload after sleep

--- src/app/modules/host/host.component.ts
+++ src/app/modules/host/host.component.ts
@@ -132,7 +132,6 @@ export class HostComponent implements OnInit {
   hideDetails: string = $localize`:@@shared.hidedetails:`;
   matcher: MediaQueryList;
   lastTime = new Date().getTime();
-  timerActive: boolean = false;
 
   get f(){
     return this.clientForm.controls;
@@ -158,10 +157,10 @@ export class HostComponent implements OnInit {
     map(() => {
       if ((new Date().getTime() - this.lastTime) > 4000) {
         console.log("reload on location");
-        this.timerActive = true;
-        if (!this.matcher.matches) {
-          this.onLocationChange(this.locationId);
-        }
+        // if (!this.matcher.matches) {
+        location.reload();
+          // this.onLocationChange(this.locationId);
+        // }
       }
       this.lastTime = new Date().getTime();
     })
@@ -587,7 +586,7 @@ export class HostComponent implements OnInit {
     this.matcher.addListener(this.screenSize);
 
     if (this.matcher.matches) {return;}
-    if (this.timerActive) { this.timerActive = false; return; }
+
     this.businessId = this.authService.businessId();
     this.userId = this.authService.userId();
 
