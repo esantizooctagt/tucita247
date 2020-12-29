8d4a5e42e3b002ba718bfb39c34a9f394335880f
Erick Santizo <esantizo@octagt.com>
2020-12-29 10:32:39 -0600

    dev branch

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,3 +1,7 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
     production: false,
     wsEndPoint: 'wss://k93m5dsm4c.execute-api.us-east-1.amazonaws.com/prod',
